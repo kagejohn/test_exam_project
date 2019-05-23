9b4a39019ecb448992f7e7340140e93d042e43cf
Christian Philip Ege Nielsen <kagejohn@kagejohn.dk>
2019-05-23 19:03:46 +0200

    removed default tests and fixed SeleniumTests

--- testExamProject/testExamProject.Tests/SeleniumTests.cs
+++ testExamProject/testExamProject.Tests/SeleniumTests.cs
@@ -10,108 +10,109 @@ using OpenQA.Selenium.Support.UI;
 
 namespace testExamProject.Tests
 {
+    [TestClass]
     class SeleniumTests
     {
-        ChromeDriver driver;
-        WebDriverWait wait;
+        private ChromeDriver _driver;
+        private WebDriverWait _wait;
 
         [TestMethod]
         public void TestConnectCourse()
         {
             string url = "http://localhost:51025/Home/ConnectCourseToSubject";
-            driver = new ChromeDriver();
-            driver.Navigate().GoToUrl(url);
-            driver.Manage().Window.Maximize();
-            driver.FindElement(By.Id("CourseID")).SendKeys("2");
-            driver.FindElement(By.Id("SubjectID")).SendKeys("3");
-            driver.FindElement(By.ClassName("Button")).Click();
-            wait = new WebDriverWait(driver, new TimeSpan(0, 0, 5));
-            wait.Until(wt => wt.FindElement(By.Id("Success:")).Equals("True"));
-            driver.Close();
-            driver.Dispose();
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl(url);
+            _driver.Manage().Window.Maximize();
+            _driver.FindElement(By.Id("CourseID")).SendKeys("2");
+            _driver.FindElement(By.Id("SubjectID")).SendKeys("3");
+            _driver.FindElement(By.ClassName("Button")).Click();
+            _wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 5));
+            _wait.Until(wt => wt.FindElement(By.Id("Success:")).Equals("True"));
+            _driver.Close();
+            _driver.Dispose();
         }
 
         [TestMethod]
         public void TestCreateCourse()
         {
             string url = "http://localhost:51025/Home/CreateCourse";
-            driver = new ChromeDriver();
-            driver.Navigate().GoToUrl(url);
-            driver.Manage().Window.Maximize();
-            driver.FindElement(By.Id("Name")).SendKeys("Bob");
-            driver.FindElement(By.Id("Duration")).SendKeys("42");
-            driver.FindElement(By.Id("AddedDate")).SendKeys("1-1-1996");
-            driver.FindElement(By.Id("TeacherId")).SendKeys("5");
-            driver.FindElement(By.ClassName("Button")).Click();
-            wait = new WebDriverWait(driver, new TimeSpan(0, 0, 5));
-            wait.Until(wt => wt.FindElement(By.Id("Success:")).Equals("True"));
-            driver.Close();
-            driver.Dispose();
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl(url);
+            _driver.Manage().Window.Maximize();
+            _driver.FindElement(By.Id("Name")).SendKeys("Bob");
+            _driver.FindElement(By.Id("Duration")).SendKeys("42");
+            _driver.FindElement(By.Id("AddedDate")).SendKeys("1-1-1996");
+            _driver.FindElement(By.Id("TeacherId")).SendKeys("5");
+            _driver.FindElement(By.ClassName("Button")).Click();
+            _wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 5));
+            _wait.Until(wt => wt.FindElement(By.Id("Success:")).Equals("True"));
+            _driver.Close();
+            _driver.Dispose();
         }
 
         [TestMethod]
         public void TestCreateSubject()
         {
             string url = "http://localhost:51025/Home/CreateSubject";
-            driver = new ChromeDriver();
-            driver.Navigate().GoToUrl(url);
-            driver.Manage().Window.Maximize();
-            driver.FindElement(By.Id("Name")).SendKeys("Benglish");
-            driver.FindElement(By.ClassName("Button")).Click();
-            wait = new WebDriverWait(driver, new TimeSpan(0, 0, 5));
-            wait.Until(wt => wt.FindElement(By.Id("Success:")).Equals("True"));
-            driver.Close();
-            driver.Dispose();
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl(url);
+            _driver.Manage().Window.Maximize();
+            _driver.FindElement(By.Id("Name")).SendKeys("Benglish");
+            _driver.FindElement(By.ClassName("Button")).Click();
+            _wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 5));
+            _wait.Until(wt => wt.FindElement(By.Id("Success:")).Equals("True"));
+            _driver.Close();
+            _driver.Dispose();
         }
 
         [TestMethod]
         public void TestCreateStudent()
         {
             string url = "http://localhost:51025/Home/CreateStudent";
-            driver = new ChromeDriver();
-            driver.Navigate().GoToUrl(url);
-            driver.Manage().Window.Maximize();
-            driver.FindElement(By.Id("Name")).SendKeys("Bobbob");
-            driver.FindElement(By.Id("Age")).SendKeys("23");
-            driver.FindElement(By.ClassName("Button")).Click();
-            wait = new WebDriverWait(driver, new TimeSpan(0, 0, 5));
-            wait.Until(wt => wt.FindElement(By.Id("Success:")).Equals("True"));
-            driver.Close();
-            driver.Dispose();
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl(url);
+            _driver.Manage().Window.Maximize();
+            _driver.FindElement(By.Id("Name")).SendKeys("Bobbob");
+            _driver.FindElement(By.Id("Age")).SendKeys("23");
+            _driver.FindElement(By.ClassName("Button")).Click();
+            _wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 5));
+            _wait.Until(wt => wt.FindElement(By.Id("Success:")).Equals("True"));
+            _driver.Close();
+            _driver.Dispose();
         }
 
         [TestMethod]
         public void TestCreateTeacher()
         {
             string url = "http://localhost:51025/Home/CreateTeacher";
-            driver = new ChromeDriver();
-            driver.Navigate().GoToUrl(url);
-            driver.Manage().Window.Maximize();
-            driver.FindElement(By.Id("Name")).SendKeys("Trevor");
-            driver.FindElement(By.Id("Education")).SendKeys("Benglish");
-            driver.FindElement(By.Id("Teaching")).Click();
-            driver.FindElement(By.ClassName("Button")).Click();
-            wait = new WebDriverWait(driver, new TimeSpan(0, 0, 5));
-            wait.Until(wt => wt.FindElement(By.Id("Success:")).Equals("True"));
-            driver.Close();
-            driver.Dispose();
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl(url);
+            _driver.Manage().Window.Maximize();
+            _driver.FindElement(By.Id("Name")).SendKeys("Trevor");
+            _driver.FindElement(By.Id("Education")).SendKeys("Benglish");
+            _driver.FindElement(By.Id("Teaching")).Click();
+            _driver.FindElement(By.ClassName("Button")).Click();
+            _wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 5));
+            _wait.Until(wt => wt.FindElement(By.Id("Success:")).Equals("True"));
+            _driver.Close();
+            _driver.Dispose();
         }
 
         [TestMethod]
         public void TestJoinCourse()
         {
             string url = "http://localhost:51025/Home/JoinCourse";
-            driver = new ChromeDriver();
-            driver.Navigate().GoToUrl(url);
-            driver.Manage().Window.Maximize();
-            driver.FindElement(By.Id("StudentID")).SendKeys("14");
-            driver.FindElement(By.Id("CourseID")).SendKeys("24");
-            driver.FindElement(By.Id("Paid")).Click();
-            driver.FindElement(By.ClassName("Button")).Click();
-            wait = new WebDriverWait(driver, new TimeSpan(0, 0, 5));
-            wait.Until(wt => wt.FindElement(By.Id("Success:")).Equals("True"));
-            driver.Close();
-            driver.Dispose();
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl(url);
+            _driver.Manage().Window.Maximize();
+            _driver.FindElement(By.Id("StudentID")).SendKeys("14");
+            _driver.FindElement(By.Id("CourseID")).SendKeys("24");
+            _driver.FindElement(By.Id("Paid")).Click();
+            _driver.FindElement(By.ClassName("Button")).Click();
+            _wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 5));
+            _wait.Until(wt => wt.FindElement(By.Id("Success:")).Equals("True"));
+            _driver.Close();
+            _driver.Dispose();
         }
     }
 }
